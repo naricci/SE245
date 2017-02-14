3dd18358dfd1a37223539eb9d4baee0ad40c0798
Nicholas Ricci <naricci@email.neit.edu>
2017-02-14 17:48:25 -0500

    create file.io class for handling CSV files

---
+++ Lab5_SavingDataInFiles/Assign2_ContactForm/FileIO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assign2_ContactForm
+{
+    class FileIO
+    {
+        public static void writeFile(string myFileName, string myString)
+        {
+            // Declare StreamWriter Object
+            StreamWriter sw;
+
+            string results = "OK";
+
+            // Try Catch Block
+            try
+            {
+                // Create StreamWriter and set filename/mode
+                sw =
+                    new StreamWriter(File.Open(myFileName,
+                    FileMode.OpenOrCreate));
+
+                // Place cursor at first column on new row at end of last record
+                sw.BaseStream.Seek(0, SeekOrigin.End);
+
+                // Write Data to bottom of File
+                sw.WriteLine(myString);
+
+                // Force the File to Write, Save and Clear Buffer
+                sw.Flush();
+
+                // Close File whether or not there was an Error
+                sw.Close();
+            }
+            catch (Exception err)
+            {
+                string x = err.Message;
+            }
+            finally
+            {
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Lab5_SavingDataInFiles/Assign2_ContactForm/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assign2_ContactForm
+{
+    public partial class frmContactForm : Form
+    {
+        string myString, salutation;
+        
+        public frmContactForm()
+        {
+            InitializeComponent();
+        }
+
+        private void frmContactForm_Load(object sender, EventArgs e)
+        {
+            // Populate combobox with items
+            cmbRelationship.Items.Add("Choose Relationship");
+            cmbRelationship.Items.Add("Spouse");
+            cmbRelationship.Items.Add("Parent");
+            cmbRelationship.Items.Add("Sibling");
+            cmbRelationship.Items.Add("Friend");
+            cmbRelationship.Items.Add("Co-worker");
+            cmbRelationship.Items.Add("Arch Nemesis");
+
+            // Set which combobox item to display as default 
+            cmbRelationship.SelectedIndex = 0;
+            cmbState.SelectedIndex = 0;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Make sure the feedback label is cleared from any previous attempts
+            lblFeedback.Text = "";
+
+            bool isValid = true;
+
+            if (isValid)
+            {
+                // Check to see which Radio Button is selected
+                if (radMr.Checked)
+                {
+                    salutation = radMr.Text;
+                }
+                else if (radMrs.Checked)
+                {
+                    salutation = radMrs.Text;
+                }
+                else
+                {
+                    salutation = radMs.Text;
+                }
+
+                // Loop through the list and add the list to the listbox on the form
+                lboxContacts.Items.Add(salutation + " " + txtFirstName.Text + " " + txtLastName.Text);
+                lboxContacts.Items.Add(txtStreet1.Text + " " + txtStreet2.Text);
+                lboxContacts.Items.Add(txtCity.Text + ", " + cmbState.SelectedItem.ToString() + ", " + txtZip.Text);
+                lboxContacts.Items.Add(txtEmail.Text);
+                lboxContacts.Items.Add(txtHomePhone.Text);
+                lboxContacts.Items.Add(txtWorkPhone.Text);
+                lboxContacts.Items.Add(txtCellPhone.Text);
+                lboxContacts.Items.Add(dtpBirthday.Value);
+                lboxContacts.Items.Add(dtpAnniversary.Value);
+                lboxContacts.Items.Add(chkCardWorthy.Text.ToString());
+                lboxContacts.Items.Add(cmbRelationship.SelectedItem.ToString());
+                lboxContacts.Items.Add(txtNotes.Text);
+
+                DateTime dtBDay = new DateTime();
+                // dtBDay = Convert.ToDateTime(dtpBirthday.Text);
+                dtBDay = dtpBirthday.Value;
+
+                // lblFeedback.Text = dtBDay.ToShortDateString();
+            }
+            
+            /**************************** FORM VALIDATION ********************************/
+
+            // First Name Validation
+            if (!Validators.IsItFilledIn(txtFirstName.Text))
+            {
+                isValid = false;
+                lblFeedback.Text += "Error:  Please enter a first name.\n";
+            }
+
+            // Last Name Validation 
+            if (!Validators.IsItFilledIn(txtLastName.Text))
+            {
+                isValid = false;
+                lblFeedback.Text += "Error:  Please enter a last name.\n";
+            }  
+
+            // State Validator
+            if (cmbState.SelectedIndex == 0)
+            {
+                isValid = false;
+                lblFeedback.Text += "Error:  Please select a state from the dropdown menu.\n";
+            }
+
+            // Zip Code Validation
+            if (!Validators.IsValidZip(txtZip.Text))
+            {
+                isValid = true;
+                lblFeedback.Text += "Error:  Must enter a valid zip code.\n";
+            }
+            
+            // Email Validation
+            if (!Validators.IsValidEmail(txtEmail.Text))
+            {
+                isValid = true;
+                lblFeedback.Text += "Error:  Please enter a valid email address.\n";
+            }
+            
+            // Home Phone Validator
+            if (!Validators.IsValidPhoneNumber(txtHomePhone.Text))
+            {
+                isValid = false;
+                lblFeedback.Text += "Error:  Please enter a valid home phone number.\n";
+            }
+
+            // Work Phone Validator
+            if (!Validators.IsValidPhoneNumber(txtWorkPhone.Text))
+            {
+                isValid = false;
+                lblFeedback.Text += "Error:  Please enter a valid work phone number.\n";
+            }
+
+            // Cell Phone Validator
+            if (!Validators.IsValidPhoneNumber(txtCellPhone.Text))
+            {
+                isValid = false;
+                lblFeedback.Text += "Error:  Please enter a valid cell phone number.\n";
+            }
+
+            /******************** Save Data to File ********************/
+
+            // create a string to gather the data
+            string contactRecord;
+
+            // start storing contact info
+            contactRecord = DateTime.Now.ToShortDateString() + "," + txtFirstName.Text + "," + txtLastName.Text;
+
+            // display record in Output Label
+            contactRecord = lblOutput.Text;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+          {
+              /** Clear the textboxes individually
+               *
+               * txtFirstName.Text = "";
+               * txtFirstName.Clear();       // Another way to clear 
+               * txtLastName.Text = "";
+               * txtStreet1.Text = "";
+               * txtStreet2.Text = "";
+               * txtCity.Text = "";
+               * txtZip.Text = "";
+               * txtEmail.Text = "";
+               * txtHomePhone.Text = "";
+               * txtWorkPhone.Text = "";
+               * txtCellPhone.Text = "";
+               * 
+               */
+
+            // Loop to clear data from the form
+            foreach (var c in this.Controls)
+            {
+                // Clear the TextBoxes
+                if (c is TextBox)
+                {
+                    ((TextBox)c).Text = String.Empty;
+                }
+                // Clear the DTPs
+                else if (c is DateTimePicker)
+                {
+                    ((DateTimePicker)c).Text = String.Empty;
+                }
+                // Clear the Checkboxes
+                else if (c is CheckBox)
+                {
+                    ((CheckBox)c).Checked = false;
+                }
+            }
+
+            // Clear the Comboboxes
+            cmbState.SelectedIndex = 0;
+            cmbRelationship.SelectedIndex = 0;
+
+            // Clear all the items from the listbox
+            lboxContacts.Items.Clear();
+
+            // Clear the Feedback Label
+            lblFeedback.Text = "";
+        }
+    }
+}
